3db82e41ebf076ad8a4c7acaa01cfcb4283e0abb
amairanysolanamejia <amairany.solanamejia@gmail.com>
2021-03-18 15:03:47 -0600

    Obtención de datos de los artículos del Banco Mundial

--- web_scrapper/main.py
+++ web_scrapper/main.py
@@ -1,11 +1,17 @@
 import argparse
 import logging
 logging.basicConfig(level=logging.INFO)
+import re
+
+from requests.exceptions import HTTPError
+from urllib3.exceptions import MaxRetryError
 
 import research_page_objects as research
 from common import config
 
 logger = logging.getLogger(__name__)
+is_well_formed_link = re.compile(r'^https?://.+/.+$')
+is_root_path = re.compile(r'^/.+$')
 
 def _research_scraper(research_site_uid):
     host=config()['research_sites'][research_site_uid]['url']
@@ -13,8 +19,37 @@ def _research_scraper(research_site_uid):
     logging.info('Beginning scraper for {}'.format(host))
     homepage = research.HomePage(research_site_uid, host)
 
+    articles = []
     for link in homepage.article_links:
-        print(link)
+        article = _fetch_article(research_site_uid, host, link)
+
+        if article:
+            logger.info('Article fetched!!! yeih')
+            articles.append(article)
+            print(article.title)
+        print(len(articles))
+
+def _fetch_article(research_site_uid, host, link):
+    logger.info('Start fetching article at {}'.format(link))
+
+    article = None
+    try:
+            article = research.ArticlePage(research_site_uid, _build_link(host,link))
+    except(HTTPError, MaxRetryError) as e:
+        logger.warning('Error while fetching the article', exc_info = False)
+
+    if article and not article.body:
+        logger.warning('Invalid article, there is no body')
+        return None
+    return article
+
+def _build_link(host, link):
+    if is_well_formed_link.match(link):
+        return link
+    elif is_root_path.match(link):
+        return '{}{}'.format(host, link)
+    else: 
+        return '{host}({uri}'.format(host=host,uri=link)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
--- web_scrapper/research_page_objects.py
+++ web_scrapper/research_page_objects.py
@@ -2,21 +2,12 @@ import bs4
 import requests
 
 from common import config
-
-class HomePage:
+class ResearchPage:
     def __init__(self,research_site_uid,url):
         self._config =config()['research_sites'][research_site_uid]
         self._queries = self._config['queries']
         self._html= None
         self._visit(url)
-
-    @property
-    def article_links(self):
-        link_list = []
-        for link in self._select(self._queries['homepage_article_links']):
-            if link and link.has_attr('href'):
-                link_list.append(link)
-        return set(link['href'] for link in link_list)
 
     def _select(self, query_string):
         return self._html.select(query_string)
@@ -27,3 +18,33 @@ class HomePage:
         response.raise_for_status()
 
         self._html=bs4.BeautifulSoup(response.text, 'html.parser')
+    
+class HomePage(ResearchPage):
+    def __init__(self,research_site_uid,url):
+        super().__init__(research_site_uid,url)
+    
+    
+    @property
+    def article_links(self):
+        link_list = []
+        for link in self._select(self._queries['homepage_article_links']):
+            if link and link.has_attr('href'):
+                link_list.append(link)
+        return set(link['href'] for link in link_list)
+
+
+class ArticlePage(ResearchPage):
+    def __init__(self, research_site_uid, url):
+        super().__init__(research_site_uid,url)
+
+    @property
+    def body(self):
+        result = self._select(self._queries['article_body'])
+
+        return result[0].text if len(result) else ''
+
+    def title(self):
+        result = self._select(self._queries['article_title'])
+
+        return result[0].text if len(result) else ''
+
